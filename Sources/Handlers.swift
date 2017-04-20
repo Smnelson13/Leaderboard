3dd02706d585b03db8fde6f6fec05abe678a7577
shane nelson <smnelson13@live.com>
2017-04-20 13:17:25 -0400

    Api using perfect Framework

---
+++ Sources/Handlers.swift
@@ -0,0 +1,88 @@
+import PerfectHTTP
+import SQLiteStORM
+import StORM
+
+func processSaveScore(request: HTTPRequest, _ response: HTTPResponse)
+{
+  response.setHeader(.contentType, value: "application/json")
+  var responseDictionary = [String: String]()
+  
+  guard let theScore = Int(request.param(name: "score")!),
+    let theUserName = request.param(name: "username"),
+    let theDisplayName = request.param(name: "displayname") else
+  {
+    response.status = .badRequest
+    responseDictionary["error"] = "Please seupply values"
+    do {
+      try response.setBody(json: responseDictionary)
+    } catch
+    {
+      print(error)
+    }
+    response.completed()
+    return
+  }
+  let aScore = Score(connect)
+  aScore.score = theScore
+  aScore.userName = theUserName
+  aScore.displayName = theDisplayName
+  
+  do {
+    try aScore.save()
+    responseDictionary["error"] = "No Error"
+  }catch
+  {
+    print(error)
+    responseDictionary["error"] = String(describing: error)
+  }
+  
+  do {
+    try response.setBody(json: responseDictionary)
+  } catch
+  {
+    print(error)
+  }
+  
+  response.completed()
+}
+
+
+
+func getHighScores(request: HTTPRequest, response: HTTPResponse)
+{
+  response.setHeader(.contentType, value: "application/json")
+  
+  var responseDictionary = [String: Any]()
+  
+  let scores = Score(connect)
+  let cursor = StORMCursor(limit: 10, offset: 0)
+
+
+  do {
+    try scores.select(columns: ["username", "displayname", "score"], whereclause: "score > :1", params: [0], orderby: ["score DESC"], cursor: cursor)
+    var resultArray = [[String: Any]]()
+    for row in scores.rows()
+    {
+      var aScoreDictionary = [String: Any]()
+      aScoreDictionary["username"] = row.userName
+      aScoreDictionary["score"] = row.score
+      aScoreDictionary["displayname"] = row.displayName
+      resultArray.append(aScoreDictionary)
+      //resultArray.append(aScoreDictionary)
+    }
+      responseDictionary["highscores"] = resultArray
+  } catch
+    {
+     print(error)
+    }
+    
+  do {
+    try response.setBody(json: responseDictionary)
+  
+  } catch //PerfectError.networkError
+  {
+  print(error)
+  }
+  response.completed()
+}
+
